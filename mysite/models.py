620cec7491d488bbe51f8b462ab0823a431e4efc
Rysiek <rychu.189@gmail.com>
2017-12-31 21:04:58 +0100

    added database

--- mysite/models.py
+++ mysite/models.py
@@ -5,15 +5,23 @@ from django.utils import timezone
 
 
 class Match(models.Model):
-    author = models.ForeignKey('auth.User')
+    matchID = models.IntegerField(primary_key=True)
+    matchID2 = models.IntegerField()
+    #author = models.ForeignKey('auth.User')
     Team1 = models.CharField(max_length=200)
     Team2 = models.CharField(max_length=200)
-    goals1 = models.IntegerField
-    goals2 = models.IntegerField
+    country1 = models.CharField(max_length=20)
+    country2 = models.CharField(max_length=20)
+    goals1 = models.IntegerField()
+    goals2 = models.IntegerField()
     matchYear=models.CharField(max_length=20)
     matchOrder=models.CharField(max_length=20)
-    gametype=models.CharField(max_length=20)
+    HomeAway=models.CharField(max_length=20)
+    matchType=models.CharField(max_length=20)
     stage = models.CharField(max_length=20)
     penalties=models.CharField(max_length=20)
-    CoinToss=models.IntegerField
-    
+    CoinToss=models.IntegerField()
+
+class Country(models.Model):
+    Team = models.CharField(max_length=200)
+    CountryTeam = models.CharField(max_length=200)
--- mysite/views.py
+++ mysite/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Match
+import xlrd
+import xlsxwriter
+import os.path
+
+
 
 # Create your views here.
+def match_details(request):
+    #print(list(Match.objects.values('matchID','matchYear','Team1','Team2').filter(CoinToss=0)))
+    data=list(Match.objects.values('matchID','matchID2','Team1','Team2').filter(matchID2=15000))
+    print(data)
+
+
+
+    return render(request, 'mysite/base_page.html', {})
