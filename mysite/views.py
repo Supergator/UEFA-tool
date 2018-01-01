ef49d265187de6524770f8b2e503270959b4584f
Rysiek <rychu.189@gmail.com>
2018-01-01 22:44:56 +0100

    added header and footer

--- mysite/views.py
+++ mysite/views.py
@@ -10,7 +10,7 @@ import os.path
 def match_details(request):
     #print(list(Match.objects.values('matchID','matchYear','Team1','Team2').filter(CoinToss=0)))
     data=list(Match.objects.values('matchID','matchID2','Team1','Team2').filter(matchID2=15000))
-    print(data)
+    #print(data)
 
 
 
