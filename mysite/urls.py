ff039e62f4b7f978f982a2aaff7709b15762cf95
Rysiek <rychu.189@gmail.com>
2018-01-03 00:07:27 +0100

    frontend

--- mysite/urls.py
+++ mysite/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url, include
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.match_details, name='match_details'),
+    url(r'^$', views.criteria, name='criteria'),
+    url(r'^matches_list.html', views.match_details, name='match_details'),
 ]
--- mysite/views.py
+++ mysite/views.py
@@ -1,17 +1,29 @@
 from django.shortcuts import render
 from .models import Match
-import xlrd
-import xlsxwriter
+#import xlrd
+#import xlsxwriter
+from django.template import loader
 import os.path
-
+from django.http import HttpResponse
 
 
 # Create your views here.
-def match_details(request):
+def criteria(request):
     #print(list(Match.objects.values('matchID','matchYear','Team1','Team2').filter(CoinToss=0)))
     data=list(Match.objects.values('matchID','matchID2','Team1','Team2').filter(matchID2=15000))
-    #print(data)
+    print(data)
+    context={'data':data}
+    print(request.session)
 
 
+    request.session['xx'] = data
 
-    return render(request, 'mysite/base_page.html', {})
+    return render(request, 'mysite/base_page.html', context)
+    l#oad_template = request.path.split('/')[-1]
+    #print(load_template)
+    #template = loader.get_template('mysite/' + 'base_page.html')
+    #return HttpResponse(template.render(context, request))
+
+def match_details(request):
+    xx = request.session.get('xx')
+    return render(request, 'mysite/matches_list.html', {'num':xx})
