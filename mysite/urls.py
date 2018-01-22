cef34b8cb969fab5601970ad2b1d91795b8f0268
Rysiek <rychu.189@gmail.com>
2018-01-23 00:43:43 +0100

    europe map

--- mysite/urls.py
+++ mysite/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     url(r'^$', views.home_page, name='home_page'),
     url(r'^favicon\.ico$', favicon_view),
     url(r'^SetCriteria/', views.SetCriteria, name='SetCriteria'),
+    url(r'^prepareDataForCountries/', views.prepareDataForCountries, name='prepareDataForCountries'),
     url(r'^GetClubs/', views.GetClubs, name='GetClubs'),
     #url(r'^matches_list\.html$', views.matches_list, name='matches_list'),
     url(r'^(\D+)$', views.load_page, name='load_page'),
--- mysite/views.py
+++ mysite/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .models import Match
 #import xlrd
 #import xlsxwriter
-from django.template import loader
+from django.template import loader, RequestContext
 import os.path
 from django.http import HttpResponse
 import re
@@ -19,7 +19,7 @@ def home_page(request):
 
 def load_page(request,a):
 
-    if bool(re.search('/map.html$', request.path)) | bool(re.search('/matches_list.html$', request.path)):
+    if bool(re.search('/matches_list.html$', request.path)):
 
         criteria = request.session.get('criteria')
         StartSelected =criteria['StartSelected']
@@ -42,11 +42,12 @@ def load_page(request,a):
 
             return render(request, 'mysite'+'/matches_list.html', {'data':data})
 
-        if bool(re.search('/map.html$', request.path)):
+    if bool(re.search('/map.html$', request.path)):
+        print(request.session.get('criteria'))
+        #CountryData = prepareDataForCountries(data,CountrySelected,ClubSelected)
 
-            CountryData = prepareDataForCountries(data,CountrySelected,ClubSelected)
+        return render(request, 'mysite'+'/map.html', {})
 
-            return render(request, 'mysite'+'/map.html', {'data':data})
     if bool(re.search('/home.html$', request.path)):
         return render(request, 'mysite'+'/home.html', {})
     #print(request.path)
@@ -102,16 +103,33 @@ def GetClubs(request):
 
     return JsonResponse({'clubs':actual_data}, safe=False)#,JsonResponse(output2, safe=False)
 
-def prepareDataForCountries(data,CountrySelected,ClubSelected):
+def prepareDataForCountries(request):
 
+    criteria = request.session.get('criteria')
+    StartSelected =criteria['StartSelected']
+    EndSelected =criteria['EndSelected']
+    CountrySelected =criteria['CountrySelected']
+    ClubSelected =criteria['ClubSelected']
+    CupSelected =criteria['CupSelected']
+    StageSelected =criteria['StageSelected']
+
+    data = Match.objects.filter(Q(countryID1=int(CountrySelected)) | Q(countryID2=int(CountrySelected))) \
+    .filter(matchYear__range=(StartSelected,EndSelected)).order_by('matchID')
+    if ClubSelected != "0":
+        data = data.filter(Q(Team1=ClubSelected) | Q(Team2=ClubSelected))
+    if CupSelected != "0":
+        data = data.filter(matchType=CupSelected)
+    if StageSelected != ['1', '2', '3', '4', '5', '6']:
+        data = data.filter(stageID__in=StageSelected)
     #ContryList = data.filter(countryID1=int(CountryCode)).values('countryID1').distinct()
     #ContryList2 = data.filter(countryID2=int(CountryCode)).values('countryID2').distinct()
     CountryData = {}
     for CountryCode in range(0,62):
-
+        #print(CountryCode)
         win = 0
         draw = 0
         lost = 0
+        ClubList = []
 
         for match in data:
             if str(CountryCode) != CountrySelected:
@@ -123,6 +141,9 @@ def prepareDataForCountries(data,CountrySelected,ClubSelected):
                         draw += 1
                     else:
                         lost += 1
+                    if match.Team2 not in ClubList:
+                        ClubList.append(match.Team2)
+
                 elif (match.countryID2 != CountryCode) & (match.countryID1 == CountryCode):
                     if match.goals1 > match.goals2:
                         lost += 1
@@ -130,11 +151,29 @@ def prepareDataForCountries(data,CountrySelected,ClubSelected):
                         draw += 1
                     else:
                         win += 1
+                    if match.Team1 not in ClubList:
+                        ClubList.append(match.Team1)
+        #CountryCode2= 'a' + str(CountryCode)
+        CountryData[str(CountryCode)]={}
 
-            CountryData[CountryCode]={'W':win,'D':draw,'L':lost}
-            #print(CountryCode)
+        if (win + draw + lost) == 0:
+            CountryData[str(CountryCode)]["enable"] =  False
+        else:
+            CountryData[str(CountryCode)]["enable"] =  True
+            if win > lost:
+                CountryData[str(CountryCode)]["color"] = '#9a9a68' #green
+            elif win == lost:
+                CountryData[str(CountryCode)]["color"] = '#59798e' #blue
+            else:
+                CountryData[str(CountryCode)]["color"] = '#B12401' #red
+
+            ClubListStr = '<br>'.join(ClubList)
+            CountryData[str(CountryCode)]["text"] = \
+            '<p>Stats for selected criteria against the clubs from the country above:<br> Wins - '+ str(win) +' <br>Draws - '+ str(draw) \
+            +'<br>Losts - '+ str(lost) +'<br><br>Opponents from this country:<br>'+ ClubListStr +'</p>'
 
 
-    print(CountryData)
-    CountryData='a'
-    return CountryData
+    #print(CountryData)
+    #CountryData='a'
+    #return CountryData
+    return JsonResponse({'data':CountryData}, safe=False)
